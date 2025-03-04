e90e177355bc1be90b0f599f47005bab580b3e07
fadli rivansyah <fadlirvnsyah@gmail.com>
2025-03-04 10:48:30 +0700

    update challenge about remove excalmation

---
+++ 04-removeExclamationMarks.js
@@ -0,0 +1,7 @@
+// function removeExclamationMarks(str) {
+//     const result = str.replace(/!/g, '');
+//     return result;
+// }
+const removeExclamationMarks = str => str.replace(/!/g, '');
+
+console.log(removeExclamationMarks('hello world!!'));
