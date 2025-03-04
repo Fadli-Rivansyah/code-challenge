515e63edeb39f5ffc25854371e5839a8471c90db
fadli rivansyah <fadlirvnsyah@gmail.com>
2025-03-04 10:01:18 +0700

    tantangan coding even or odd

---
+++ 04-Even-or-Odd.js
@@ -0,0 +1,12 @@
+// function evenOrOdd(number){
+//     let result;
+//     if(number % 2 ==0) {
+//         result = 'Even';
+//     }else{
+//         result = 'Odd';
+//     }
+//     return result;
+// }
+
+const evenOrOdd = (number) => number%2==0 ? 'Even' : 'Odd';
+console.log(evenOrOdd(1));
